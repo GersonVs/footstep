3ce9aad47e3c2fd088e3b4675b945379981b39c6
GersonVs <ichigokurosaki.ga65@gmail.com>
2019-08-09 12:53:27 -0300

    Fist Commit

---
+++ lib/main.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/semantics.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: "FootStep",
+    home: PrimeiraPagina(),
+  ));
+}
+
+class PrimeiraPagina extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: new Image.asset(
+          'lib/assets/FootStepBig.png',
+          fit: BoxFit.cover,
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          onPressed: () {},
+          color: Colors.black,
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.notifications_none),
+            onPressed: () {},
+            color: Colors.black,
+          ),
+        ],
+        elevation: 6,
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Container(
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    width: 180,
+                    height: 80,
+                    margin: EdgeInsets.only(left: 15, right: 15, top: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[200],
+                          blurRadius: 3,
+                        )
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Image.asset('lib/assets/running.png'),
+                        ),
+                        Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 10, left: 10),
+                              child: Text(
+                                "Total de passos",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: 180,
+                    height: 80,
+                    margin: EdgeInsets.only(left: 15, right: 15, top: 20),
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.all(Radius.circular(12)),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey[200],
+                          blurRadius: 3,
+                        )
+                      ],
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: EdgeInsets.only(left: 10),
+                          child: Image.asset('lib/assets/alarm.png'),
+                        ),
+                        Align(
+                            alignment: Alignment.topCenter,
+                            child: Padding(
+                              padding: EdgeInsets.only(top: 10, left: 10),
+                              child: Text(
+                                "Tempo parado",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 12,
+                                ),
+                              ),
+                            ))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 330,
+              height: 330,
+              margin: EdgeInsets.only(top: 30, right: 20 ),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.all(Radius.circular(360)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    spreadRadius: 5,
+                  )
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(left: 69, top: 20),
+                    child: Image.asset(
+                      'lib/assets/grupo1.png',
+                      scale: 0.78,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: 120,
+              height: 80,
+              margin: EdgeInsets.only(left: 200, ),
+              decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.all(Radius.circular(50)),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black,
+                    spreadRadius: 4,
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
