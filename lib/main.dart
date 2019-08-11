2697353b0676e8a2eed18f72c200026e5f273013
GersonVs <ichigokurosaki.ga65@gmail.com>
2019-08-11 18:43:23 -0300

    mudança

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'SegundaPagina.dart';
 
 void main() => runApp(MyApp());
 
+
+
+
 class MyApp extends StatelessWidget {
   
   @override
@@ -15,16 +18,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+
+
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-
 
   final String title;
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
+
+
+
+
+
 
 class _MyHomePageState extends State<MyHomePage> {
 
